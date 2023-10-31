c5c25e2a19f8fc7b30e0d5779efe75335fadf138
Navid Pazoki <navid.pazoki@gmx.de>
2023-10-31 18:15:34 +0100

    add comments to js Login

--- script/login.js
+++ script/login.js
@@ -48,7 +48,7 @@ let PwdHTML = /*html*/`
         <form action="https://join-615.developerakademie.net/php/send_mail_change_pwd.php" method="POST"> 
             <input type="email" id="email" name="Email" placeholder="Email" required>
             <span id="msgMail"></span>
-            <button class="but-dark">Send me the email</button>
+            <button class="but-dark" onclick ="forgotPwd()" >Send me the email</button>
         </form>
 `;
 let SendMailHTML = /*html*/`
@@ -67,12 +67,22 @@ let ChangePwdHTML = /*html*/`
     </div>
 `;
 
-
+/**
+ * Renders HTML content to a specified element with the given ID.
+ *
+ * @param {string} id - The ID of the target element where the HTML will be rendered.
+ * @param {string} HTML - The HTML content to be rendered.
+ */
 function renderHTML(id, HTML) {
     let contentBox = document.getElementById(id);
     contentBox.innerHTML = HTML;
 }
 
+/**
+ * Toggles the visibility of a password input field between "password" and "text" and updates its background.
+ *
+ * @param {string} id - The ID of the password input element to toggle.
+ */
 function togglPwd(id) {
     let element = document.getElementById(id);
     let type = element.getAttribute('type') === 'password' ? 'text' : 'password';
@@ -81,6 +91,11 @@ function togglPwd(id) {
     showPwdBg(id);
 }
 
+/**
+ * Updates the background of a password input field based on its visibility status.
+ *
+ * @param {string} id - The ID of the password input element to update the background for.
+ */
 function showPwdBg(id) {
     let element = document.getElementById(id);
     let type = element.getAttribute('type');
@@ -88,6 +103,11 @@ function showPwdBg(id) {
     else element.setAttribute("style", "background-image:url(./img/icons/visibility_off.svg);");
 }
 
+/**
+ * Hides the visibility of a password input field and updates its background.
+ *
+ * @param {string} id - The ID of the password input element to hide.
+ */
 function hidePwd(id) {
     let element = document.getElementById(id);
     element.setAttribute('type', 'password');
@@ -100,76 +120,152 @@ function hidePwd(id) {
 // this part should be in the backend
 let users = [];
 
+/**
+ * Asynchronously initializes the login process by fetching user data and contact list.
+ *
+ * @returns {Promise<void>} - A Promise that resolves when the initialization is complete.
+ */
 async function initLogin() {
     users = await getItem('users');
+    mapUsers();
     contactListSorted = await getItem('contacts');
     loadLogin();
     checkState();
 }
 
+/**
+ * Maps the existing user data to a new array of User objects with updated properties.
+ */
+function mapUsers() {
+    users = users.map(user => new User(user.name, user.email, user.pwd, user.id));
+}
+
+/**
+ * Checks if the passwords match and either returns a specified function or displays an error message.
+ *
+ * @param {Function} doFunction - The function to return or execute if the passwords match.
+ * @returns {void|Function} - If passwords match, returns the specified function; otherwise, displays an error message.
+ */
 function checkPwd(doFunction) {
     let pwd1 = document.getElementById('pwd');
     let pwd2 = document.getElementById('pwdCon');
-    if (pwd1.value === pwd2.value) {
-        doFunction;
-    } else {
-        addMsg('pwdCon', 'msgPwd', 'Password confirmation is wrong!');
-    }
+    if (pwd1.value === pwd2.value) return doFunction;
+    else return addMsg('pwdCon', 'msgPwd', 'Password confirmation is wrong!');
 }
 
+/**
+ * Adds a new user to the system if the email is unique; otherwise, displays an error message.
+ */
 function addUser() {
-    let name = document.getElementById('name');
-    let email = document.getElementById('email');
-    let pwd = document.getElementById('pwd');
-    if (!users.find(u => u.email == email.value)) {
-        addUserToData(name.value, email.value, pwd.value);
-        return
-    }
-    if (users.find(u => u.email == email.value)) {
-        addMsg('email', 'msgEmail', 'Email already exists!');
-    }
+    let newUser = getUserFromDocument();
+    if (!users.find(u => u.email == newUser.email)) return addUserToData(newUser);
+    else addMsg('email', 'msgEmail', 'Email already exists!');
 }
 
-async function addUserToData(name, email, pwd) {
-    let id = users.length + 1;
-    users.push({ id: id, name: name, email: email, pwd: pwd });
+/**
+ * Retrieves user information from the document and creates a new User object.
+ *
+ * @returns {User} - A User object containing the user's name, email, and password.
+ */
+function getUserFromDocument() {
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let pwd = document.getElementById('pwd').value;
+    return new User(name, email, pwd)
+}
+
+/**
+ * Adds a new user to the data, updates the user ID, saves the data, sends a welcome email,
+ * initiates the login process, and renders the login UI (User Interface).
+ *
+ * @param {Object} user - The user object to be added to the data.
+ * @property {string} user.name - The name of the user.
+ * @property {string} user.email - The email of the user.
+ * @property {string} user.pwd - The password of the user.
+ */
+async function addUserToData(user) {
+    user.id = users.length + 1;
+    users.push(user);
     setItem('users', users);
-    const data = JSON.stringify({ name: name, mail: email });
-    fetch("https://join-615.developerakademie.net/php/send_mail_add_user.php", { method: 'POST', body: data });
+    sendWelcomMail(user);
     await initLogin();
+    renderLogin();
+}
+
+/**
+ * Sends a welcome email to the specified user by making a POST request to a remote server.
+ *
+ * @param {Object} user - The user object to whom the welcome email will be sent.
+ * @property {string} user.name - The name of the user.
+ * @property {string} user.email - The email of the user.
+ */
+function sendWelcomMail(user) {
+    const data = JSON.stringify({ name: user.name, mail: user.email });
+    fetch("https://join-615.developerakademie.net/php/send_mail_add_user.php", { method: 'POST', body: data });
+}
+
+/**
+ * Renders the login UI (User Interface) by updating the HTML content of specified elements and showing/hiding elements.
+ */
+function renderLogin() {
     renderHTML('content', LoginHTML);
     showElementID('signUp');
     renderHTML('hidden', SendMailHTML);
 }
 
+/**
+ * function to check the Login 
+ */
 function login() {
-    let email = document.getElementById('email');
-    let pwd = document.getElementById('pwd');
-    let user = users.find(u => u.email == email.value && u.pwd == pwd.value);
-    if (user) {
-        safeLogin(JSON.stringify(user));
-        console.log('login ok');
-        window.location.href = 'pages/summary.html' + '?user=' + user.id;
-    }
-    else {
-        addMsg('pwd', 'msgPwd', 'Ups, wrong password! Try again.');
-    }
+    let user = checkUser();
+    user ? loginSuccess(user) : addMsg('pwd', 'msgPwd', 'Ups, wrong password! Try again.');
 }
 
+/**
+ * Checks user credentials by creating a temporary user object with email and password from the document,
+ * and then finding a matching user in the existing user data.
+ *
+ * @returns {User|null} - If a matching user is found, returns the user object; otherwise, returns null.
+ */
+function checkUser() {
+    let loginUser = new User;
+    loginUser.email = document.getElementById('email').value;
+    loginUser.pwd = document.getElementById('pwd').value;
+    return users.find(u => u.email == loginUser.email && u.pwd == loginUser.pwd);
+}
+
+/**
+ * Redirects the user to the summary page.
+ * Safely logs in the user and redirects the user to the summary page with the user's ID as a query parameter.
+ *
+ * @param {Object} user - The user object representing the logged-in user.
+ * @property {string} user.id - The unique identifier of the user.
+ */
+function loginSuccess(user) {
+    safeLogin(JSON.stringify(user));
+    window.location.href = 'pages/summary.html' + '?user=' + user.id;
+}
+
+/**
+ * Redirects the user to the summary page as a guest.
+ */
 function loginGuest() {
-    // set activ User!
     window.location.href = 'pages/summary.html';
 }
 
+/**
+ * Safely manages user login information in local storage based on the user's preference.
+ *
+ * @param {string} user - The serialized user information to be stored in local storage.
+ */
 function safeLogin(user) {
     let save = document.getElementById('saveLogin');
-    if (save.checked) {
-        localStorage.setItem('User', user);
-    } else {
-        localStorage.removeItem('User');
-    }
+    save.checked ? localStorage.setItem('User', user) : localStorage.removeItem('User');
 }
 
+/**
+ * Loads user login information from local storage and populates the login form if available.
+ */
 function loadLogin() {
     let user = JSON.parse(localStorage.getItem('User'));
     if (user) {
@@ -179,17 +275,23 @@ function loadLogin() {
     }
 }
 
+/**
+ * Initiates the process for password recovery. 
+ * Checks if the entered email exists in the user data, 
+ * renders the appropriate HTML content, and displays relevant messages.
+ */
 function forgotPwd() {
     let email = document.getElementById('email');
     if (users.find(u => u.email == email.value)) {
         renderHTML('hidden', SendMailHTML);
         renderHTML('content', LoginHTML);
         showElementID('signUp');
-    } else {
-        addMsg('email', 'msgMail', 'Email not exist!');
-    }
+    } else addMsg('email', 'msgMail', 'Email not exist!');
 }
 
+/**
+ * Checks the state of the URL parameters and performs actions based on the state.
+ */
 function checkState() {
     const msg = URL_PARAMS.get('msg');
     if (msg == 'send_Mail') {
